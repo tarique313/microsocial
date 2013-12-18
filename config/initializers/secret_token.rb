9a74b991c89b779cb86abe26b84d6cb866b14ecc
tarique313 <mahmud.tarique@gmail.com>
2013-12-18 10:59:16 +0600

    Add Signup, Signin and navbar - Day 6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microsocial::Application.config.secret_token = '1373e526cad02196829e01845563af3f924e336cafb02e696cf241ebee05720380d92f8d38263f74488dc94fac1bf2816c82c99ddbb2109ff9a6ed0dbbd3614e'
